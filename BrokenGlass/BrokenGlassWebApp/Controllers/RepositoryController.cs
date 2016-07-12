26f9736838c36a5cf2fdeac280546150a2a46444
Anton <muzychenkoaa@gmail.com>
2016-07-12 17:05:59 +0600

    -- Add download distrib

---
+++ BrokenGlass/BrokenGlassWebApp/Controllers/RepositoryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrokenGlassWebApp.Controllers
+{
+    public class RepositoryController : Controller
+    {
+
+        // GET: Repository
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public FileResult getIOSPackage()
+        {
+            string fileName = "ios-broken-glass.ipa";
+            string path = getPathPackage(fileName);
+            byte [] array = System.IO.File.ReadAllBytes(path);
+            return File(array, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+        public FileResult getIOSPackageName(string fileName)
+        {
+            string path = getPathPackage(fileName);
+            byte[] array = System.IO.File.ReadAllBytes(path);
+            return File(array, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+
+        private string getPathPackage(string namePackage)
+        {
+            return Path.Combine(HttpContext.Server.MapPath("~/Repository"), namePackage);
+        }
+    }
+}
